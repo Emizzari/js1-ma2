83f529d64b2c2098c08f1fc41fa874f724da2d82
Emizzari <emilie@tonnessen.com>
2020-01-28 14:29:17 +0100

    Q5 - adding answer
    Its not correct yet, need to find a way to target data attributes in js

--- emilie-tonnessen-js1-ma2.js
+++ emilie-tonnessen-js1-ma2.js
@@ -73,6 +73,14 @@
 
    <button class="btn" data-animal="dog">Hover over me</button> */
 
+    const varOne = document.querySelector("button");
+
+    function functionOne() {
+        varOne.classList.remove("hover");
+    }
+
+    varOne.addEventListener("mouseout", functionOne);
+
 
 
 // QUESTION 6
