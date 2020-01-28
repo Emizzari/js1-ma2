7509df43591eb7de84188c9029caa0a565d91b93
Emizzari <emilie@tonnessen.com>
2020-01-28 13:34:31 +0100

    Q3 - fixing question description

--- emilie-tonnessen-js1-ma2.js
+++ emilie-tonnessen-js1-ma2.js
@@ -34,9 +34,10 @@
 
 // QUESTION 3
 /* Refer: lesson 2
-   Select the input in the HTML below by its id. Add a keydown event 
-   listener. The callback function passed to the event listener should 
-   log the value of the key that was pressed.
+   - Select the input in the HTML below by its id. 
+   - Add a keydown event listener. 
+   - The callback function passed to the event listener should 
+     log the value of the key that was pressed.
 
    <input class="input" id="firstName"></div> /*
 
