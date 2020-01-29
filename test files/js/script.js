468969ccc0237da72ae211f2a32718de15f8341f
Emizzari <emilie@tonnessen.com>
2020-01-29 14:55:16 +0100

    Test files updated

--- test files/js/script.js
+++ test files/js/script.js
@@ -1,12 +1,23 @@
+
+
+
+
+
+
 // QUESTION 10
 /* Refer: lesson 4
    - Select the div from the HTML below.
-   - Write code that updates the div's text to say Text updated after
+   - Write code that updates the div's text to say "Text updated" after
      waiting 2 seconds. 
    - You can use either innerText or innerHTML to update
      the text.
 
    <div class="container">Placeholder text</div> */
 
-   const selectContainer = document.querySelector(".container");
+const container = document.querySelector(".container");
 
+function updateContainer() {
+    container.innerHTML = "Text updated";
+}
+
+setTimeout(updateContainer, 2000);
